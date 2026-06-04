1cdc2004fd6690bdc9a8056a9f199fc0a5939076
Kavithasuji <sujithakavi20@gmail.com>
2026-06-04 22:13:30 +0530

    Initial React setup

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+// import RegisterPage from "../pages/RegisterPage";
+// import ChatPage from "../pages/ChatPage";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        {/* <Route path="/register" element={<RegisterPage />} />
+        <Route path="/chat" element={<ChatPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
